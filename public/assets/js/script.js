e6f0e63e434bbd18a8c32a7f99560768f1ddaa4d
jennanmiles <jennanmiles@gmail.com>
2020-02-26 19:55:13 -0700

    adding assets folder with d3 chart js and html files

---
+++ public/assets/js/script.js
@@ -0,0 +1,34 @@
+$(window).load(function() {
+    
+    let queryUrl = 'https://publicgis.tucsonaz.gov/open/rest/services/OpenData/OpenData_PublicSafety/MapServer/48/query?where=1%3D1&outFields=*&outSR=4326&f=json';
+
+    // 2020 api call
+    // https://publicgis.tucsonaz.gov/open/rest/services/OpenData/OpenData_PublicSafety/MapServer/54/query?where=1%3D1&outFields=*&outSR=4326&f=json
+
+
+    $.ajax({
+        url: queryUrl,
+        method: 'GET',
+        dataType: 'jsonp'
+    }).then(function(response) {
+            console.log('hit?');
+            console.log(response);
+            
+            for (let i = 0; i < 100; i++) {
+                // unix time stamp -- had to convert           
+                let date = new Date(response.features[i].attributes.DATE_REPT);
+
+                let realdate = $('<p>').text('Date: ' + date);
+
+                // description of event
+                let desc = $('<p>').text('Description: ' + response.features[i].attributes.STATUTDESC);
+
+                // address of event
+                let addy = $('<p>').text('Address: ' + response.features[i].attributes.ADDRESS_PUBLIC);
+                
+                $('.list-data').append(realdate,desc,addy);
+                $('.list-data').append('<div class="liner"></div>')
+            }
+        }
+    )
+});
