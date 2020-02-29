553163b70c2c3fccce91d0d6de6492c452edf577
Andrew Bergheim <andrewbergheim@gmail.com>
2020-02-29 03:33:51 -0700

    jawsDB connection

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 let mysql = require("mysql");
-
-let connection = mysql.createConnection({
+let connection;
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+}
+else{
+    connection = mysql.createConnection({
     port: 3306,
     host: "localhost",
     user: "root",
     password: "yourRootPassword",
     database: "projectTwo"
   });
+}
 
   connection.connect(function(err) {
     if (err) {
