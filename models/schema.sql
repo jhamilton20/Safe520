95b648b694706da96576dd0d635704a889ec6409
Andrew Bergheim <andrewbergheim@gmail.com>
2020-02-29 03:47:29 -0700

    updated schema

--- models/schema.sql
+++ models/schema.sql
@@ -4,8 +4,9 @@ CREATE database if not exists projectTwo;
 USE projectTwo ;
 
 CREATE TABLE if not exists logins(
-    id int primary key AUTO_INCREMENT,
+    id int primary key AUTO_INCREMENT NOT NULL,
     username varchar (250) NOT NULL,
     email varchar(40) NOT NULL,
-    pw varchar (250) NOT NULL
+    pw varchar (250) NOT NULL,
+    createdAt TIMESTAMP NOT NULL Default CURRENT_TIMESTAMP
   );
